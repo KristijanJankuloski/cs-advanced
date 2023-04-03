a8ef4baa62f5b2ad1430ec0781bc0fff946bf66b
Kristijan <jankuloski.k@gmail.com>
2023-04-03 22:15:01 +0200

    finish exercise 1

---
+++ ClassExercises/Interfaces/Entities/Teacher.cs
@@ -0,0 +1,19 @@
+﻿using Interfaces.Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.Entities
+{
+    public class Teacher : User, ITeacher
+    {
+        public string Subject { get; set; }
+
+        public override void PrintUser()
+        {
+            Console.WriteLine($"{Id}, {Name} {Username} - {Subject}");
+        }
+    }
+}
--- ClassExercises/Interfaces/Program.cs
+++ ClassExercises/Interfaces/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Interfaces.Entities;
+using Interfaces.Entities.Interfaces;
+
+List<IUser> users = new List<IUser>();
+Teacher teacher1 = new Teacher() { Id=1 ,Name = "Kristijan", Username = "kiko", Password = "1234", Subject="CS" };
+Teacher teacher2 = new Teacher() { Id = 2, Name = "John", Username = "jd", Password = "password", Subject = "Networking" };
+Student student = new Student() { Id = 3, Name = "Bob", Username = "bob", Password = "12345", Grades = new List<int> { 5, 4, 5, 3} };
+
+users.Add(teacher1);
+users.Add(teacher2);
+users.Add(student);
+
+foreach(var user in users)
+{
+    user.PrintUser();
+}
