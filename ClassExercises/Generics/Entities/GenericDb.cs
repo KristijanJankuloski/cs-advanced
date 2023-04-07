782d654d619c795e19ed9bd069404022e369b8f0
Kristijan <jankuloski.k@gmail.com>
2023-04-07 18:00:39 +0200

    Generics homework

---
+++ ClassExercises/Generics/Entities/GenericDb.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics.Entities
+{
+    public class GenericDb<T> where T : BaseEntity
+    {
+        public List<T> List { get; set; }
+        public GenericDb()
+        {
+            List = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            List.Add(item);
+        }
+
+        public void PrintItems()
+        {
+            foreach (var item in List)
+            {
+                Console.WriteLine(item.GetInfo());
+            }
+        }
+    }
+}
---
+++ ClassExercises/Generics/Program.cs
@@ -0,0 +1,9 @@
+﻿using Generics.Entities;
+
+GenericDb<Order> orders = new GenericDb<Order>();
+
+orders.Add(new Order(1, "Phone"));
+orders.Add(new Order(2, "Tablet"));
+orders.Add(new Order(3, "Shavers"));
+
+orders.PrintItems();
